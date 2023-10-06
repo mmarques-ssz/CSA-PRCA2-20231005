6143baf813de6f670abad1bf023d54257940bec7
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2023-10-05 21:15:31 -0300

    Add files via upload

---
+++ projLeds/projLeds/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projLeds
+{
+    public partial class Form1 : Form
+    {
+
+        private Leds l;
+        public Form1()
+        {
+            InitializeComponent();
+            l = new Leds();
+            label1.Text = l.getEstado().ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            l.acender(1);
+            label1.Text = l.getEstado().ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            l.acender(8);
+            label1.Text = l.getEstado().ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            l.acender(2);
+            label1.Text = l.getEstado().ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            l.acender(3);
+            label1.Text = l.getEstado().ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            l.acender(4);
+            label1.Text = l.getEstado().ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            l.acender(5);
+            label1.Text = l.getEstado().ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            l.acender(6);
+            label1.Text = l.getEstado().ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            l.acender(7);
+            label1.Text = l.getEstado().ToString();
+        }
+    }
+}
---
+++ projLeds/projLeds/Leds.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projLeds
+{
+    internal class Leds
+    {
+        private byte dado;
+        public Leds()
+        {
+            this.dado = 0;
+        }
+        public Leds(byte dado)
+        {
+            this.dado = dado;
+        }
+        public void setDado(byte dado)
+        {
+            this.dado = dado;
+        }
+        public void acender(int led)
+        {
+            this.dado = (byte)((int)this.dado | (int)Math.Pow(2, led - 1));
+        }
+        public void apagar(int led)
+        {
+            this.dado = (byte)((int)this.dado & (int)(255 - Math.Pow(2, led - 1)));
+        }
+        public bool getEstado(int led)
+        {
+            return false;
+        }
+
+        public byte getEstado()
+        {
+            return this.dado;   
+        }
+
+    }
+}
